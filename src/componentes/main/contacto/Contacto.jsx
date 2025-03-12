a3bd055e14ffc13d2661277aa67d2fd11672ff9c
Christian <christian_96_09@hotmail.com>
2025-03-12 20:06:53 -0300

    linkedin en seccion contacto

--- src/componentes/main/contacto/Contacto.jsx
+++ src/componentes/main/contacto/Contacto.jsx
@@ -1,10 +1,17 @@
+import { redesSociales } from "../../../dataProps/redesSociales"
+import './contacto.css'
 
 const Contacto = () => {
+  const linkLinkedin = redesSociales.find(r=>r.txt==="Linkedin").ruta
   return (
     <main>
-      <form action="" style={{textAlign:"center", padding:"30%"}}>
-        <code>Proximamente</code>
-      </form>
+      {/* <form action="" style={{textAlign:"center", padding:"30%"}}>
+      </form> */}
+        <a href={linkLinkedin} className={`btn linkedin`} target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-linkedin"></i>
+          Perfil de Linkedin
+        </a>
+
     </main>
   )
 }
