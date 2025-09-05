f84c3bec3706e288421190439600aa63bec1064c
Christian <christian_96_09@hotmail.com>
2025-09-05 14:29:26 -0300

    Agregar componente BtnCopiar y copiar mail en contacto

--- src/componentes/main/contacto/Contacto.jsx
+++ src/componentes/main/contacto/Contacto.jsx
@@ -1,4 +1,5 @@
 import { redesSociales } from "../../../dataProps/redesSociales"
+import BtnCopiar from "../BtnCopiar"
 import './contacto.css'
 
 const Contacto = () => {
@@ -11,10 +12,13 @@ const Contacto = () => {
 
       <div className="contacto-contenedor">
 
-        <a href={linkMail} className={`btn btn-mail`} title={mail} target="_blank" rel="noopener noreferrer">
+        <span className="btn btn-mail">
           <i className="fa-solid fa-envelope"></i>
-          Enviar Mail
-        </a>
+          <a href={linkMail} style={{flexGrow:"1"}} title={mail} target="_blank" rel="noopener noreferrer">
+            Enviar Mail
+          </a>
+          <BtnCopiar texto={mail} />
+        </span>
 
         <a href={linkLinkedin} className={`btn linkedin`} target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-linkedin"></i>
